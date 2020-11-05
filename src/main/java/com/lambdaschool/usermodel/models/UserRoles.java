5343b9cf27a34992ee16e01c47afcea0af0b2e7a
Shun Chiang <yuushamenma@gmail.com>
2020-11-04 19:24:09 -0600

    finished userroles

--- src/main/java/com/lambdaschool/usermodel/models/UserRoles.java
+++ src/main/java/com/lambdaschool/usermodel/models/UserRoles.java
@@ -1,4 +1,41 @@
 package com.lambdaschool.usermodel.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table
 public class UserRoles {
+    @Id
+    @ManyToOne
+    @JoinColumn(name="userid")
+    private User user;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name="roleid")
+    private Role role;
+
+    public UserRoles() {
+    }
+
+    public UserRoles(User user, Role role) {
+        this.user = user;
+        this.role = role;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
