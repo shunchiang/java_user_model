f8a72c91a4ee0d724143540af33c50e765d318c3
Shun Chiang <yuushamenma@gmail.com>
2020-11-08 18:27:20 -0600

    added jsonignore to user roles

--- src/main/java/com/lambdaschool/usermodel/models/UserRoles.java
+++ src/main/java/com/lambdaschool/usermodel/models/UserRoles.java
@@ -1,5 +1,7 @@
 package com.lambdaschool.usermodel.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 import java.io.Serializable;
 
@@ -10,11 +12,13 @@ public class UserRoles implements Serializable {
     @Id
     @ManyToOne
     @JoinColumn(name="userid")
+    @JsonIgnoreProperties(value = "roles", allowSetters = true)
     private User user;
 
     @Id
     @ManyToOne
     @JoinColumn(name="roleid")
+    @JsonIgnoreProperties(value = "users", allowSetters = true)
     private Role role;
 
     public UserRoles() {
