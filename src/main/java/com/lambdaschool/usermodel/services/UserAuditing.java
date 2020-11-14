5ef49591edcf678263b103885885d064637755c7
Shun Chiang <yuushamenma@gmail.com>
2020-11-14 13:08:08 -0600

    added component to userauditing

--- src/main/java/com/lambdaschool/usermodel/services/UserAuditing.java
+++ src/main/java/com/lambdaschool/usermodel/services/UserAuditing.java
@@ -1,9 +1,11 @@
 package com.lambdaschool.usermodel.services;
 
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
+@Component
 public class UserAuditing implements AuditorAware<String> {
     @Override
     public Optional<String> getCurrentAuditor() {
